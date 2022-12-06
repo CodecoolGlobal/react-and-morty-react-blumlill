38c2242bc279dfb10b1c514d3508ad9b8639cbfe
Botond Lilla <thiguff@gmail.com>
2022-12-06 10:37:31 +0100

    remove unnecessary files

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -1,9 +1,7 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-
-
   return <div className="App">Take a look at the console! (F12)</div>;
 }
 
