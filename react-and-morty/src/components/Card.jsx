a8ff18a2edd90f8beedd3d5408f9fcedd150bfae
galiptai <galiptai@gmail.com>
2022-12-07 10:37:23 +0100

    small fix

--- react-and-morty/src/components/Card.jsx
+++ react-and-morty/src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ data, type }) {
   const charater = type === 'characters';
-  let source = charater ? data.image : `../images/r&m_loc${Math.ceil(Math.random() * 8)}.png`;
+  const source = charater ? data.image : `../images/r&m_loc${Math.ceil(Math.random() * 8)}.png`;
   return (<div className="Card">
     <img src={source} alt={data.name}></img>
     <p>Name: {data.name}</p>
