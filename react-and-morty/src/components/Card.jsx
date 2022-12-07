52ae363dd06ee597e0d7377b593d8768e3651fc1
galiptai <galiptai@gmail.com>
2022-12-07 10:35:00 +0100

    add images to locations

--- react-and-morty/src/components/Card.jsx
+++ react-and-morty/src/components/Card.jsx
@@ -1,7 +1,8 @@
 export default function Card({ data, type }) {
   const charater = type === 'characters';
+  let source = charater ? data.image : `../images/r&m_loc${Math.ceil(Math.random() * 8)}.png`;
   return (<div className="Card">
-    {charater && <img src={data.image} alt={data.name}></img>}
+    <img src={source} alt={data.name}></img>
     <p>Name: {data.name}</p>
     {charater && <p>Species: {data.species}</p>}
     {!charater && <p>Type: {data.type}</p>}
