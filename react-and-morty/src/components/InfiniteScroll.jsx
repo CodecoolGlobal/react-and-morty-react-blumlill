0c1b9705a8838ddf61f74ff5183c8e74cc7da92c
galiptai <galiptai@gmail.com>
2022-12-08 16:12:14 +0100

    start toggle

--- react-and-morty/src/components/InfiniteScroll.jsx
+++ react-and-morty/src/components/InfiniteScroll.jsx
@@ -4,15 +4,18 @@ const InfiniteScroll = (props) => {
   return (
     <div className={null === props.results ? 'hidden' : ''}>
       <span>Infinite scrolling</span>
-      <input
-        type="checkbox"
-        onChange={() => {
-          props.setInfinite(!props.infinite);
-          props.setResults([]);
-          props.setFetchType({ ...props.fetchType, page: 1 });
-        }}
-        checked={props.infinite}
-      />
+      <div className='toggle'>
+        <input
+          type="checkbox"
+          onChange={() => {
+            props.setInfinite(!props.infinite);
+            props.setResults([]);
+            props.setFetchType({ ...props.fetchType, page: 1 });
+          }}
+          checked={props.infinite}
+        />
+        <span className='slider'></span>
+      </div>
     </div>
   );
 };
