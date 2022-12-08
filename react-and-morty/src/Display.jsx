c9101ba2a1be4c374596ac9f824292daff3e9fd5
silentstorm902 <jozsefkiss90@gmail.com>
2022-12-08 11:38:26 +0100

    created modal

--- react-and-morty/src/Display.jsx
+++ react-and-morty/src/Display.jsx
@@ -1,24 +1,33 @@
-const Display = ({ data, type }) => {
+import './Display.css';
+
+const Display = ({ data, type, visible, setVisible }) => {
   const charater = type === 'characters';
 
+  function changeModal() { 
+    setVisible(!visible);
+    console.log(visible);
+  }
+
   return (
-    <div className="modal">
-      <p>Name: {data.name}</p>
-      {charater ? (
-        <>
-          <p>Species: {data.species}</p>
-          <p>Gender: {data.gender}</p>
-          <p>Status: {data.status}</p>
-          <p>Location: {data.location.name}</p>
-          <p>Appeared in {data.episode.length} episodes</p>
-        </>
-      ) : (
-        <>
-          <p>Type: {data.type}</p>
-          <p>Dimension: {data.dimension}</p>
-          <p>Residents: {data.residents.length}</p>
-        </>
-      )}
+    <div className={'modal'} style={{display: visible ? 'block' : 'none'}} onClick={() => {changeModal();}}>
+      <div className='modal-view'>
+          <p>Name: {data.name}</p>
+          {charater ? ( 
+            <>
+              <p>Species: {data.species}</p>
+              <p>Gender: {data.gender}</p>
+              <p>Status: {data.status}</p>
+              <p>Location: {data.location.name}</p>
+              <p>Appeared in {data.episode.length} episodes</p>
+            </>
+          ) : (
+            <>
+              <p>Type: {data.type}</p>
+              <p>Dimension: {data.dimension}</p>
+              <p>Residents: {data.residents.length}</p>
+            </>
+          )}
+      </div>
     </div>
   );
 };
