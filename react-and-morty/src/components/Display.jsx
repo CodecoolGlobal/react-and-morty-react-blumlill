1777207788b367a88e552dde38b3fe4921fc8cee
Botond Lilla <thiguff@gmail.com>
2022-12-08 21:10:21 +0100

    design display

--- react-and-morty/src/components/Display.jsx
+++ react-and-morty/src/components/Display.jsx
@@ -3,32 +3,41 @@ import './Display.css';
 const Display = ({ data, type, visible, setVisible }) => {
   const charater = type === 'characters';
 
-  function changeModal() { 
+  function changeModal() {
     setVisible(!visible);
     console.log(visible);
   }
 
   return (
-    <div className={'modal'} style={{display: visible ? 'block' : 'none'}} onClick={() => {changeModal();}}>
-      <div className='modal-view'>
-          <p>Name: {data.name}</p>
-          {charater ? ( 
-            <>
-            <img src={data.image} style={{width:'30%'}} alt="" />
-              <p>Species: {data.species}</p>
-              <p>Gender: {data.gender}</p>
-              <p>Status: {data.status}</p>
-              <p>Location: {data.location.name}</p>
-              <p>Appeared in {data.episode.length} episodes</p>
-            </>
-          ) : (
-            <>
-              <img src={`../images/r&m_loc${data.image}.png`} style={{width:'30%'}} alt="" />
-              <p>Type: {data.type}</p>
-              <p>Dimension: {data.dimension}</p>
-              <p>Residents: {data.residents.length}</p>
-            </>
-          )}
+    <div
+      className={'modal'}
+      style={{ display: visible ? 'block' : 'none' }}
+      onClick={() => {
+        changeModal();
+      }}
+    >
+      <div className="modal-view">
+        <p className="name">
+          {data.name.split('').join(' ')} {console.log(data.name.split(''))}{' '}
+          {console.log(data.name.split('').join(' '))}
+        </p>
+        {charater ? (
+          <>
+            <img src={data.image} style={{ width: '30%' }} alt="" />
+            <p>Species: {data.species}</p>
+            <p>Gender: {data.gender}</p>
+            <p>Status: {data.status}</p>
+            <p>Location: {data.location.name}</p>
+            <p>Appeared in {data.episode.length} episodes</p>
+          </>
+        ) : (
+          <>
+            <img src={`../images/r&m_loc${data.image}.png`} style={{ width: '30%' }} alt="" />
+            <p>Type: {data.type}</p>
+            <p>Dimension: {data.dimension}</p>
+            <p>Residents: {data.residents.length}</p>
+          </>
+        )}
       </div>
     </div>
   );
